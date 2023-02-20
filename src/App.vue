48503459e5ff998d576855a3db713528b376b83c
Daniel Venera <61143114+Daniel-Venera@users.noreply.github.com>
2023-02-20 09:38:29 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import * as Scry from "scryfall-sdk";
+
+// ...in some function somewhere...
+const chalice = await Scry.Cards.byName("Chalice of the Void");
+console.log(chalice.name, chalice.set); // "Chalice of the Void", "a25"
+
+const prints = await chalice.getPrints();
+console.log(prints.length); // 7
+</script>
+
+<template></template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
